40c8ded4190be732a3106dcf4ee780a9deab0587
Anduin <2864595564@qq.com>
2018-11-26 15:42:47 +0800

    底部导航栏修改

--- app/src/main/java/com/example/thinkpad/icompetition/view/activity/impl/MainActivity.java
+++ app/src/main/java/com/example/thinkpad/icompetition/view/activity/impl/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.bv_me:
                 mViewPager.setCurrentItem(1);
                 mToolbar.setTitle("我的");
+
                 break;
         }
     }
